c52b30a178e4e7d31dcc0f51f90dc73dfd998c15
hopeforever <48434805+QTazimi@users.noreply.github.com>
2023-08-16 12:41:16 +0800

    Update runme.sh

--- bash/runme.sh
+++ bash/runme.sh
@@ -2,7 +2,6 @@
 
 echo '--start--'
 
-#"/home/HBV/SRA335342/SRR3104446/bio_bert/SRR3104446.hbvhuman.sam"
 path=/home/HBV/SRA335342/
 ls $path | while read file
 do
@@ -18,6 +17,7 @@ do
   --vocab_file=/home/hbv/program/config/word2index.txt \
   --bert_config_file=/home/hbv/program/config/bert_config.json \
   --init_checkpoint=/home/HBV/maxpooltest/class_output/model.ckpt-68000 \
+  --region_path= /home/program/VirolSDC/file/region.csv \
   --fasta_path=/home/hbv/ref/GRCh38/GRCh37_latest_genomic.fna \
   --bam_path=${path}${file}/bio_bert/${file}fil_sort.bam \
   --threads=30 \
